a5db680c4b060bef4732b3522aad9a8b3e1d7053
toddrun <toddrun@gmail.com>
2018-03-24 14:59:03 -0400

    Support lastModifiedStart & lastModifiedEnd for GET calls

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # Metrc Wrapper - interact with Metrc in your Node application #
 
 Interacting with Metrc's API requires certification. You'll need API keys to use the library.
+
+# What the heck is Metrc anyway???
+
+Metrc is the Cannabis Tracking System (CTS) used in most States where recreational cannabis is legal 
+(and even in some that still only offer Medical Marijuana). Metrc is built and maintained by Franwell Inc.,
+and is only accessible to licensees, verified integrators and other authorized entities. 
+
+# What's the Metrc API?
+
+While Metrc includes a Web based interface for licensess to interact with, it also includes an API to 
+allow software vendors to programmatically update Metrc data. The API does not necassarily provide
+all of the functionality of the Web based interface, but is the only way 3rd parties can report data 
+into Metrc. 
+
+# Why can't I just start using the API? What possible benefit could this open source project provide?
+
+Every software vendor in the Cannabis space is currently writing their own code for accessing and interacting
+with the Metrc API. This library is not intended to be a production ready replacement for writing your own
+code (though you're welcome to use it). It provides a partial implementation to highlight some of the 
+peculiarities others have encountered, hopefully saving some of your sanity in the process.
+
+For example, when you POST a new record, the API returns a status code of 200 . . . and that's it. Since you'll
+likely need to determine the "Id" of the record you just created, an empty response payload is less than
+optimal. So, this library wraps each POST with a corresponding GET, and logic to extract the newly introduced
+record. Likewise, the GET endpoints force you to page through data based on the "LastModified" field, and only
+allow a short time span (beginning 4/1/2018 requests are limited to no more than a 24 hour period). This library
+provides a way to make multiple calls in a single method call (though be careful . . . call limits and throttling
+rules are always subject to change and could cause issues).
+
+
+
--- test/MetrcTest.js
+++ test/MetrcTest.js
@@ -1,25 +1,31 @@
 'use strict'
 
-const Metrc = require('../lib/Metrc');
+const Metrc = require('../lib/Metrc')
 
-const nock = require('nock');
-const assert = require('assert');
+const nock = require('nock')
+const assert = require('assert')
+const moment = require('moment')
+const _ = require('lodash')
 
 describe('Metrc', () => {
-  const domain = 'https://api-xx.metrc.com';
-  const licenseNumber = 'ABC-X000X';
-  const apiKey = 'abcdefghijklmnopqrstuvwxyz1234567890';
-  const userKey = '1234567890abcdefghijklmnopqrstuvwxyz';
+  const domain = 'https://api-xx.metrc.com'
+  const licenseNumber = 'ABC-X000X'
+  const apiKey = 'abcdefghijklmnopqrstuvwxyz1234567890'
+  const userKey = '1234567890abcdefghijklmnopqrstuvwxyz'
   
   const path = '/any/v1/endpoint';
   
   describe('get', () => {
+    beforeEach(() => {
+      nock.cleanAll()
+    })
+    
     it('hits the endpoint as expected', (done) => {
       const metrcCall = nock(domain)
         .get(path)
         .query({licenseNumber: licenseNumber})
         .basicAuth({user: apiKey, pass: userKey})
-        .reply(200, "OK");
+        .reply(200, [])
       
       new Metrc(domain, licenseNumber, apiKey, userKey)
         .get(path)
@@ -27,6 +33,160 @@ describe('Metrc', () => {
           assert.ok(metrcCall.isDone());
           done();
        })
+    })
+    
+    it('returns the payload from metrc', (done) => {
+      const metrcCall = nock(domain).get(path).query(true).reply(200, [{foo: 'bar'}])
+      
+      new Metrc(domain, licenseNumber, apiKey, userKey)
+        .get(path)
+        .then((results) => {
+          assert.deepEqual([{foo: 'bar'}], results)
+          done();
+       })
+    })
+    
+    it('includes multiple payloads if more than one call is needed', (done) => {
+      const startDate = moment().subtract(4, 'day').subtract(10, 'hours').toJSON()
+      const endDate = moment().subtract(1, 'day').toJSON()
+      const dates = []
+      const includesFoo = (results, value) => {
+        return results.find((object) => {
+          return object.foo == value
+        })
+      }
+
+      const metrcCall = nock(domain)
+        .get(path).query(true).reply(200, [{foo: "one"}])
+        .get(path).query(true).reply(200, [{foo: "two"}])
+        .get(path).query(true).reply(200, [])
+        .get(path).query(true).reply(200, [{foo: "three"}, {foo: "four"}])
+
+      new Metrc(domain, licenseNumber, apiKey, userKey)
+        .get(path, {lastModifiedStart: startDate, lastModifiedEnd: endDate})
+        .then((results) => {
+          assert.equal(4, results.length)
+          assert.ok(includesFoo(results, "one"))
+          assert.ok(includesFoo(results, "two"))
+          assert.ok(includesFoo(results, "three"))
+          assert.ok(includesFoo(results, "four"))
+          done();
+       }).catch((e) => {console.log("ERR", e)})
+    })
+    
+    it('errors if any call fails', (done) => {
+      const startDate = moment().subtract(4, 'day').subtract(10, 'hours').toJSON()
+      const endDate = moment().subtract(1, 'day').toJSON()
+      const dates = []
+      const includesFoo = (results, value) => {
+        return results.find((object) => {
+          return object.foo == value
+        })
+      }
+
+      const metrcCall = nock(domain)
+        .get(path).query(true).reply(200, [{foo: "one"}])
+        .get(path).query(true).reply(200, [{foo: "two"}])
+        .get(path).query(true).reply(504, "barf")
+
+      new Metrc(domain, licenseNumber, apiKey, userKey)
+        .get(path, {lastModifiedStart: startDate, lastModifiedEnd: endDate})
+        .then(() => {
+          assert.fail("Should not have completed")
+        }).catch((expected) => {
+          assert.ok("You should put something here in case the calls fail!")
+          done()
+        })
+    })
+    
+    context('with lastModifiedStart', () => {
+      it('gets every 24 hour period from the start until the current time', (done) => {
+        const startDate = moment().subtract(47, 'hours').toJSON()
+        const assertCalls = (queryArguments) => {
+          if (queryArguments.lastModifiedStart == startDate) {
+            assert.equal(
+             moment(startDate).toJSON(),
+             moment(queryArguments.lastModifiedStart).toJSON()
+            )
+          } else {
+            assert.equal(
+              moment(queryArguments.lastModifiedStart).toJSON(), 
+              moment(queryArguments.lastModifiedEnd).subtract(24, 'hours').toJSON()
+            )
+          }
+          return true
+        }
+        
+        const metrcCall = nock(domain)
+          .get(path).query(assertCalls).reply(200, [])
+          .get(path).query(assertCalls).reply(200, [])
+        
+        new Metrc(domain, licenseNumber, apiKey, userKey)
+          .get(path, {lastModifiedStart: startDate})
+          .then(() => {
+            assert.ok(metrcCall.isDone());
+            done();
+         }).catch((e) => {console.log("ERR", e)})
+      })
+    })
+    
+    context('with lastModifiedEnd', () => {
+      it('gets 24 hour period prior to lastModifiedEnd', (done) => {
+        const endDate = moment('2018-03-17T10:19:00Z').toJSON()
+        const expectedStartDate = moment(endDate).subtract(24, 'hours').toJSON()
+        const metrcCall = nock(domain)
+          .get(path)
+          .query({
+            licenseNumber: licenseNumber, 
+            lastModifiedStart: expectedStartDate, 
+            lastModifiedEnd: endDate
+          })
+          .basicAuth({user: apiKey, pass: userKey})
+          .reply(200, []);
+        
+        new Metrc(domain, licenseNumber, apiKey, userKey)
+          .get(path, {lastModifiedEnd: endDate})
+          .then(() => {
+            assert.ok(metrcCall.isDone());
+            done();
+         }).catch((e) => {console.log("ERR", e)})
+      })
+    })
+    
+    context('with lastModifiedStart and lastModifiedEnd', () => {
+      it('calls each period between the start and end without gaps', (done) => {
+        const startDate = moment().subtract(7, 'day').subtract(14, 'hours').toJSON()
+        const endDate = moment().subtract(2, 'day').subtract(2, 'hours').toJSON()
+        const dates = []
+        const assertCalls = (queryArguments) => {
+          dates.push({
+            startDate: queryArguments.lastModifiedStart,
+            endDate: queryArguments.lastModifiedEnd
+          })
+          return true
+        }
+        
+        const metrcCall = nock(domain)
+          .get(path).query(assertCalls).reply(200, [])
+          .get(path).query(assertCalls).reply(200, [])
+          .get(path).query(assertCalls).reply(200, [])
+          .get(path).query(assertCalls).reply(200, [])
+          .get(path).query(assertCalls).reply(200, [])
+          .get(path).query(assertCalls).reply(200, [])
+        
+        new Metrc(domain, licenseNumber, apiKey, userKey)
+          .get(path, {lastModifiedStart: startDate, lastModifiedEnd: endDate})
+          .then(() => {
+            const sortedDates = _.sortBy(dates, 'startDate')
+            assert.equal(sortedDates[0].startDate, startDate)
+            assert.equal(sortedDates[sortedDates.length - 1].endDate, endDate)
+            for (let i = 0 ; i < sortedDates.length - 1; i++) {
+              assert.equal(sortedDates[i].endDate, sortedDates[i + 1].startDate)
+            }
+            assert.ok(metrcCall.isDone());
+            done();
+         }).catch((e) => {console.log("ERR", e)})
+      })
     })
   })
   
@@ -99,6 +259,21 @@ describe('Metrc', () => {
           assert.ok(metrcCall.isDone());
           done();
        })
+    })
+  })
+  
+  describe('error handling', () => {
+    it('deals with errors', (done) => {
+       const metrcCall = nock(domain)
+        .get(/.*/)
+        .replyWithError("Something went wrong");
+      
+      new Metrc(domain, licenseNumber, apiKey, userKey)
+        .get(path)
+        .catch(() => {
+          assert.ok(metrcCall.isDone());
+          done();
+      })
     })
   })
 })
